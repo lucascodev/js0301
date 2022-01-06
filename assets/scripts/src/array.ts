9da2db7f7d07ec5a7ef64eb3487b66f37b80f70b
Inpuro <sopragames.sf@hotmail.com>
2022-01-05 23:16:41 -0300

    Aula 3

---
+++ assets/scripts/src/array.ts
@@ -0,0 +1,11 @@
+const fields = {
+  name: "Glaucio Daniel",
+  site: "wwww.hcode.com.br",
+  phone: "11 2222-2222",
+  "email[1]": "email@gmail.com",
+  "email[2]": "email@gmail.com",
+  "email[3]": "email@gmail.com",
+};
+
+console.log("antes", Object.keys(fields).length);
+console.log("antes", Object.keys(fields).length);
--- assets/scripts/src/schedules-new.ts
+++ assets/scripts/src/schedules-new.ts
@@ -3,45 +3,74 @@ import endOfMonth from "date-fns/endOfMonth";
 import startOfMonth from "date-fns/startOfMonth";
 import addMonths from "date-fns/addMonths";
 import subMonths from "date-fns/subMonths";
-import locale from "date-fns/locale/pt-BR"
+import locale from "date-fns/locale/pt-BR";
+import { addDays, differenceInSeconds, endOfWeek, startOfWeek } from "date-fns";
 
 const page = document.querySelector("#schedules-new");
 
 if (page) {
+  const hoje = new Date();
+  let inicioMes = startOfMonth(hoje);
+  const fimMes = endOfMonth(hoje);
 
-    const hoje = new Date();
-    let inicioMes = startOfMonth(hoje);
-    const fimMes = endOfMonth(hoje);
+  const btnHoje = page.querySelector(".btn-today") as HTMLButtonElement;
+  const btnAnterior = page.querySelector(".btn-prev") as HTMLButtonElement;
+  const btnProximo = page.querySelector(".btn-next") as HTMLButtonElement;
+  const titulo = page.querySelector("h2") as HTMLHeadingElement;
+  const calendar = page.querySelector(".days") as HTMLUListElement;
 
-    const btnHoje = page.querySelector(".btn-today") as HTMLButtonElement;
-    const btnAnterior = page.querySelector(".btn-prev") as HTMLButtonElement;
-    const btnProximo = page.querySelector(".btn-next") as HTMLButtonElement;
-    const titulo = page.querySelector("h2") as HTMLHeadingElement;
-    const calendar = page.querySelector(".days") as HTMLUListElement;
- 
+  const render = () => {
+    titulo.innerText = format(inicioMes, "MMMM yyyy", { locale });
 
-    const render = () => {
+    calendar.innerHTML = "";
 
-        titulo.innerText = format(inicioMes, "MMMM yyyy", { locale });
+    let diaCorrente = startOfWeek(inicioMes);
+    const ultimoCalendario = endOfWeek(endOfMonth(inicioMes));
 
+    while (differenceInSeconds(ultimoCalendario, diaCorrente) > 0) {
+      const li = document.createElement("li");
+      li.innerText = format(diaCorrente, "d");
+      if (format(diaCorrente, "yyyyMM") < format(inicioMes, "yyyyMM")) {
+        li.classList.add("month-prev");
+      }
+      if (format(diaCorrente, "yyyyMM") > format(inicioMes, "yyyyMM")) {
+        li.classList.add("month-next");
+      }
+      if (format(diaCorrente, "yyyyMMdd") === format(hoje, "yyyyMMdd")) {
+        li.classList.add("active");
+      }
+
+      li.addEventListener("click", (e: Event) => {
+        const selected = calendar.querySelector(".selected");
+        if (selected) {
+          selected.classList.remove("selected");
+        }
+        const myLi = e.target as HTMLLabelElement;
+
+        myLi.classList.add("selected");
+        const input = document.querySelector("[name=schedule_at]") as HTMLInputElement;
+        input.value = myLi.dataset.schedule ?? "";
+
+      });
+      calendar.appendChild(li);
+      diaCorrente = addDays(diaCorrente, 1);
     }
+  };
 
-    btnProximo.addEventListener("click", ()=> {
-        inicioMes = addMonths(inicioMes, 1);
-        render();
-    });
-
-    btnAnterior.addEventListener('click', () => {
-        inicioMes = subMonths(inicioMes, 1);
-        render();
-    });
-
-    btnHoje.addEventListener("click", () => {
-        inicioMes = startOfMonth(hoje);
-        render();
-    });
-    
-
+  btnProximo.addEventListener("click", () => {
+    inicioMes = addMonths(inicioMes, 1);
     render();
+  });
 
+  btnAnterior.addEventListener("click", () => {
+    inicioMes = subMonths(inicioMes, 1);
+    render();
+  });
+
+  btnHoje.addEventListener("click", () => {
+    inicioMes = startOfMonth(hoje);
+    render();
+  });
+
+  render();
 }
